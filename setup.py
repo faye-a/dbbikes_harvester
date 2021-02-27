4ecdce5d5076dbf59319763a26ea0a4287f77154
Faye Arejola <faye.arejola98@gmail.com>
2021-02-27 02:03:45 +0000

    reimplementing setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='dublinbikesapp',
+    version='1.1.1',
+    author='fayea',
+    author_email='faye.arejola98@gmail.com',
+    install_requires=['requests','sqlalchemy','mysql-connector-python'],
+    description='DATA scraper for dubilnbikes'
+)
